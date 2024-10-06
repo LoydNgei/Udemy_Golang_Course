51962c3716881278f5aeb7f0e84929f044ee1606
Loyd Ngei <loydngei@gmail.com>
2024-10-06 15:22:25 +0300

    Practice file added on the same

---
+++ Project_1/repeat.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var fname, lname string
+	var YOB int
+	fmt.Print("First Name: ")
+	fmt.Scanln(&fname)
+	fmt.Print("Last Name: ")
+	fmt.Scanln(&lname)
+	fmt.Print("Year of birth: ")
+	fmt.Scanln(&YOB)
+
+	ThisYear := time.Now().Year()
+
+	ageNow := ThisYear - YOB
+	fmt.Printf("Your name is %v %v and you are %d years old\n", fname, lname, ageNow)
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module AgeCalculator
-
-go 1.22.2
