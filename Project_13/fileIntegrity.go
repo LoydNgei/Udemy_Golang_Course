bb6ec0ffd564961d0e8613e83b815ff0b7790a04
Loyd Ngei <loydngei@gmail.com>
2024-10-26 18:43:27 +0300

    file integrity project

--- Project_13/fileIntegrity.go
+++ Project_13/fileIntegrity.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"os"
+)
+
+func hash_content(content []byte) string {
+	hasher := sha256.New()
+	hasher.Write(content)
+	result := hasher.Sum(nil)
+	hash := hex.EncodeToString(result)
+	return hash
+
+}
+
+// f9d95eaccd32a46809ce0fd33384a0c595678f6459e884cf9f5b1b32c44de308
+
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: ./fileIntegrity <filename> <originalhash>")
+		return
+	}
+
+	file_name := os.Args[1]
+	original_hash := os.Args[2]
+
+	contents, err := os.ReadFile(file_name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	hash := hash_content(contents)
+
+	if hash == original_hash {
+		fmt.Println("The file is intact")
+	} else {
+		fmt.Println("The file has been tampered with")
+	}
 
 }
